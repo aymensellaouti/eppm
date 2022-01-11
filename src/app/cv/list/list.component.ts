957338934a1eabd20495a8c29fcca3cd30472942
aymen2782 <aymen.sellaouti@gmail.com>
2022-01-11 09:16:02 +0100

    end cvTech v1

--- src/app/cv/list/list.component.ts
+++ src/app/cv/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Cv} from "../model/cv";
 
 @Component({
@@ -12,9 +12,13 @@ export class ListComponent implements OnInit {
     new Cv(2,'drira', 'ahmed', 'Dev', 'rotating_card_profile3.png', '12345', 20),
     new Cv(3,'Ben Hlima', 'amal', 'Dev', 'rotating_card_profile.png', '1234', 19),
   ];
+  @Output() forwardSelectedCv = new EventEmitter<Cv | null>();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  forwardCv(cv: Cv | null) {
+    this.forwardSelectedCv.emit(cv);
+  }
 }
