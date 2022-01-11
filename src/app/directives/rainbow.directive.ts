4710608d445a295a0b5f7f75fcb7c87588f679f0
aymen2782 <aymen.sellaouti@gmail.com>
2022-01-11 11:33:51 +0100

    directives

---
+++ src/app/directives/rainbow.directive.ts
@@ -0,0 +1,16 @@
+import {Directive, HostBinding, HostListener} from '@angular/core';
+
+@Directive({
+  selector: '[appRainbow]'
+})
+export class RainbowDirective {
+  @HostBinding('style.color') color = 'black';
+  constructor() { }
+  @HostListener('keyup') onKeyUp() {
+    this.color = this.getRandomColor();
+  }
+  getRandomColor(): string {
+    return '#' + Math.floor(Math.random()*16777215).toString(16);
+  }
+
+}
